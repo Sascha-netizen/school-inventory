e7289771fc5e235508101d79763cdbcbd510561d
Sascha-netizen <sascha.r.klement@gmail.com>
2025-11-18 18:48:43 +0100

    Add InventorySystem class with selection logic for users - library or supplies

--- run.py
+++ run.py
@@ -13,9 +13,30 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("school_inventory_system")
 
 
+class InventorySystem:
+    """
+    Allows the user to choose which inventory subsystem - library or supplies - to enter
+    """
+    def __init__(self, sheet):
+        self.sheet = sheet
+    
+    def choose_inventory(self):
+        print("Which inventory would you like to access?")
+        print("[1] Library")
+        print("[2] Supplies")
 
-# Testing connection to API/Google sheets
-supplies_data = SHEET.worksheet("Supplies").get_all_values()
-print("\nSupplies Data:")
-for row in supplies_data:
-    print(row)
+        while True:
+            choice = input("Enter 1 or 2: ").strip()
+            if choice == "1":
+                return "Library"
+            elif choice == "2":
+                return "Supplies"
+            
+            else:
+                print("Your choice is invalid. Please choose 1 or 2.") 
+
+if __name__ == "__main__":
+    inventory_system = InventorySystem(SHEET)
+    selected_inventory = inventory_system.choose_inventory()
+    print(f"You selected: {selected_inventory}")
+    
