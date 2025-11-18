feefe627bb7ebfa5a9dd445f9778e16ab181e9e6
Sascha-netizen <sascha.r.klement@gmail.com>
2025-11-18 19:06:50 +0100

    Install colorama and import into programme

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,22 +24,22 @@ class InventorySystem:
         self.sheet = sheet
     
     def choose_inventory(self):
-        print("Which inventory would you like to access?")
-        print("[1] Library")
-        print("[2] Supplies")
+        print(Fore.BLUE + Style.BRIGHT + "Which inventory would you like to access?")
+        print(Fore.YELLOW + "[1] Library")
+        print(Fore.YELLOW + "[2] Supplies")
 
         while True:
-            choice = input("Enter 1 or 2: ").strip()
+            choice = input(Fore.GREEN + "Enter 1 or 2: ").strip()
             if choice == "1":
                 return "Library"
             elif choice == "2":
                 return "Supplies"
             
             else:
-                print("Your choice is invalid. Please choose 1 or 2.") 
+                print(Fore.RED + Style.BRIGHT + "Your choice is invalid. Please choose 1 or 2.") 
 
 if __name__ == "__main__":
     inventory_system = InventorySystem(SHEET)
     selected_inventory = inventory_system.choose_inventory()
-    print(f"You selected: {selected_inventory}")
+    print(Fore.MAGENTA + f"You selected: {selected_inventory}")
     
