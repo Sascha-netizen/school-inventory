5024a6da439dde935a50de55f0e76fb7eb9a4f0a
Sascha-netizen <sascha.r.klement@gmail.com>
2025-11-19 17:58:50 +0100

    Add choice logic/options for the supplies department (1-5)

--- run.py
+++ run.py
@@ -23,7 +23,12 @@ class InventorySystem:
     def __init__(self, sheet):
         self.sheet = sheet
     
+    
+    
     def choose_inventory(self):
+        """
+        Displays main inventory options and allows user to choose between either library or supplies.
+        """
         print(Fore.BLUE + Style.BRIGHT + "\nWhich inventory would you like to access?\n")
         print(Fore.YELLOW + "[1] Library")
         print(Fore.YELLOW + "[2] Supplies")
@@ -40,12 +45,15 @@ class InventorySystem:
                 print(Fore.RED + Style.BRIGHT + "\nYour choice is invalid. Please choose 1 or 2.\n") 
 
     def option_one_library(self):
+        """
+        Option one allows users to access and manage the library records.
+        """
         print(f"\nYou are now managing the Library\n")
         print("[1] Add book")
         print("[2] Update existing book")
         print("[3] Display book")
         print("[4] Delete book")
-        print("[Exit / End of session]")
+        print("[5] Exit / End of session")
 
         while True:
             choice = input("Select an option [1-5]: ").strip()
@@ -63,12 +71,48 @@ class InventorySystem:
             else:
                 print("Invalid choice. Please choose options [1] to [5]")
 
+
+    def option_two_supplies(self):
+        """
+        Option two allows users to access and manage the records of the supplies department.
+        """
+        print(f"\nYou are now managing the Supplies\n")
+        print("[1] Add item")
+        print("[2] Update existing item")
+        print("[3] Display item")
+        print("[4] Delete item")
+        print("[5] Exit / End of session")
+
+        while True:
+            choice = input("Select an option [1-5]: ").strip()
+            if choice == "1":
+                print("Add item selected")
+            elif choice == "2":
+                print("Update item selected")
+            elif choice == "3":
+                print("Display existing item selected")
+            elif choice == "4":
+                print("Delete existing item selected")
+            elif choice == "5":
+                print("End of session selected")
+                break
+            else:
+                print("Invalid choice. Please choose options [1] to [5]")
+
+        
+
 if __name__ == "__main__":
     inventory_system = InventorySystem(SHEET)
-    selected_inventory = inventory_system.choose_inventory()
-    print(Fore.MAGENTA + f"\nYou selected: {selected_inventory}")
 
-    if selected_inventory == "Library":
-        inventory_system.option_one_library()
+    while True:
+        selected_inventory = inventory_system.choose_inventory()
+        print(Fore.MAGENTA + f"\nYou selected: {selected_inventory}")
+
+        if selected_inventory == "Library":
+            inventory_system.option_one_library()
+
+        elif selected_inventory == "Supplies":
+            inventory_system.option_two_supplies()
+
 
     
