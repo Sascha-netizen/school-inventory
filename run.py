c07997c3f3623853fa75d11367bcd15194175a44
Sascha-netizen <sascha.r.klement@gmail.com>
2025-11-19 17:41:11 +0100

    Add library choice option (1-5)

--- run.py
+++ run.py
@@ -39,8 +39,36 @@ class InventorySystem:
             else:
                 print(Fore.RED + Style.BRIGHT + "\nYour choice is invalid. Please choose 1 or 2.\n") 
 
+    def option_one_library(self):
+        print(f"\nYou are now managing the Library\n")
+        print("[1] Add book")
+        print("[2] Update existing book")
+        print("[3] Display book")
+        print("[4] Delete book")
+        print("[Exit / End of session]")
+
+        while True:
+            choice = input("Select an option [1-5]: ").strip()
+            if choice == "1":
+                print("Add book selected")
+            elif choice == "2":
+                print("Update existing book selected")
+            elif choice == "3":
+                print("Display existing book selected")
+            elif choice == "4":
+                print("Delete existing book selected")
+            elif choice == "5":
+                print("End of session selected")
+                break
+            else:
+                print("Invalid choice. Please choose options [1] to [5]")
+
 if __name__ == "__main__":
     inventory_system = InventorySystem(SHEET)
     selected_inventory = inventory_system.choose_inventory()
     print(Fore.MAGENTA + f"\nYou selected: {selected_inventory}")
+
+    if selected_inventory == "Library":
+        inventory_system.option_one_library()
+
     
